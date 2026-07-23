43724019fc147db1a4b8f44f50eca4f532eb1e38
London <adamslondon351@gmail.com>
2026-07-23 09:19:00 -0400

    Initial Commit

---
+++ TargetIT/TargetITApp.swift
@@ -0,0 +1,17 @@
+//
+//  TargetITApp.swift
+//  TargetIT
+//
+//  Created by JOURNi Student on 7/23/26.
+//
+
+import SwiftUI
+
+@main
+struct TargetITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
